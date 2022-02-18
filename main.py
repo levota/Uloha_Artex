6c6ec4d286e66076c7ccfe1564cfa34b2a1a9927
levota <helmes.me@gmail.com>
2022-02-18 14:40:42 +0100

    FInished version with short explenation. Importing random is only necesary for creating random list

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import random
 
 def sum_1d_array(numbers):
    r =len(numbers)
-   prev_num=[numbers[0]]
+   new_list=[numbers[0]]
 
    for i in range(1,r):
       numbers[i]+=numbers[i-1]
-      prev_num.append(numbers[i])
-   return prev_num
+      new_list.append(numbers[i])
+   return new_list
 
 zadani = []
 for i in range(1000):
@@ -16,10 +16,8 @@ for i in range(1000):
 nums = [1,2,4,8,9,15,17]
 print(sum_1d_array(nums))
 
-
-
-
-
-
-
-
+# Program goes through list of number via for cycle.
+# It starts new list with first number of original list.
+# Then it takes next number and sum it with previous one. It goes until for cycle ends.
+# I used zadani as example of creating random list of nums containing only one numeral.
+# Example nums is used just to show it on shorter list.
